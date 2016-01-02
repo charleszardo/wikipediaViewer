a33c785553249a3b01c180890b27add0225ce6e8
Dave Charnuska <davecharnuska@gmail.com>
2016-01-02 16:25:52 -0500

    get title, description, and link from API call

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
+	function addArticle(title, desc, link) {
+		$("#results").append
+	}
+	
+	function populateResults(results) {
+		var term = results[0],
+				titles = results[1],
+				descs = results[2],
+				links = results[3],
+				len = titles.length;
+		
+		for (var i = 0; i < len; i++) {
+			
+		}
+	}
+	
+	function wikipediaSearch(str, limit) {
+		var baseUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=",
+				query = encodeURIComponent(str),
+				limit = "&limit=" + (limit || 10),
+				urlTrail = "&namespace=0&format=json",
+				fullUrl = baseUrl + query + limit + urlTrail;
+		
+		$.ajax({
+		  dataType: "jsonp",
+		  url: fullUrl,
+		  success: function(data) {
+				populateResults(data);
+		  }
+		});
+	}
+	
 	$("#submit").click(function() {
-		var v = $("#query").val();
-		console.log(v);
+		var queryString = $("#query").val();
+		wikipediaSearch(queryString);
 	})
 })
