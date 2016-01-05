fc085784fc9e0bd56a566005ca670745592fda52
Dave Charnuska <davecharnuska@gmail.com>
2016-01-05 00:06:19 -0500

    clean up

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 	var randomUrl;
 	
-  var currentRecs = [];
-	
 	function OpenInNewTab(url) {
 	  var win = window.open(url, '_blank');
 	  win.focus();
@@ -15,19 +13,6 @@ $(document).ready(function() {
 			 "<div class='desc'>" + desc + "</div>" +
 			"</a></div>"
 		)
-	}
-	
-	function recommendations(results) {
-		var titles = results[1],
-				links = results[3],
-				len = titles.length;
-				
-				for (var i = 0; i < len; i++) {
-					var title = titles[i],
-							link = links[i]
-							console.log(currentRecs.includes(title));
-							currentRecs.push(title);
-				}
 	}
 	
 	function populateResults(results) {
@@ -71,13 +56,12 @@ $(document).ready(function() {
 	function randomRedirect(results) {
 		var links = results[3],
 					url = links[0];
-				
+				// sets global randomUrl to current random url
 				randomUrl = url;
 				$("#random").prop("disabled",false);
 	}
 	
 	function wikipediaSearch(str, random, limit, dropdown) {
-		console.log('firin!');
 		var random = (random || false),
 				baseUrl = "https://en.wikipedia.org/w/api.php",
 				search = "&search=" + str,
@@ -85,9 +69,7 @@ $(document).ready(function() {
 				namespace = "&namespace=0",
 				limit = "&limit=" + (limit || 10),
 				action = "?action=opensearch",
-				fullUrl;
-				
-	 		 	fullUrl = baseUrl + action + search + limit + namespace + urlTrail;
+				fullUrl = baseUrl + action + search + limit + namespace + urlTrail;
 		
 		$.ajax({
 		  dataType: "jsonp",
@@ -105,11 +87,6 @@ $(document).ready(function() {
 		});
 	}
 	
-	$("#query").keyup(function() {
-		var val = $("#query").val();
-		// wikipediaSearch(val, false, 10, true);
-	}) 
-	
 	$("#submit").click(function() {
 		var queryString = $("#autocomplete").val();
 		wikipediaSearch(queryString, false, 10, false);
@@ -119,8 +96,6 @@ $(document).ready(function() {
 		OpenInNewTab(randomUrl);
 		randomPage();
 	})
-	
-	randomPage();
 	
 	$("#query").autocomplete({
 	    source: function(request, response) {
@@ -139,36 +114,6 @@ $(document).ready(function() {
 	    }
 	});
 	
-	// $(function() {
-	//     var availableTags = [
-	//       "ActionScript",
-	//       "AppleScript",
-	//       "Asp",
-	//       "BASIC",
-	//       "C",
-	//       "C++",
-	//       "Clojure",
-	//       "COBOL",
-	//       "ColdFusion",
-	//       "Erlang",
-	//       "Fortran",
-	//       "Groovy",
-	//       "Haskell",
-	//       "Java",
-	//       "JavaScript",
-	//       "Lisp",
-	//       "Perl",
-	//       "PHP",
-	//       "Python",
-	//       "Ruby",
-	//       "Scala",
-	//       "Scheme"
-	//     ];
-	//     $( "#query" ).autocomplete({
-	//       source: currentRecs
-	//     });
-	//   });
-	// $( "#tags" ).autocomplete({
-// 		source: currentRecs
-// 	});
+	// call randomPage on page load so that random url is ready almost immediately
+	randomPage();
 })
